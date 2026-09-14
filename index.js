9ff67d756bec48f4d70202e58f7d6eeba609a9a1
Jorge Perez <jorge.fernando.perez18@gmail.com>
2026-09-14 17:17:14 -0300

    Actualizacion mensaje pedido por profesor

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-const express = require('express');const app = express();const port = process.env.PORT || 3000;app.get('/', (req, res) => {  res.send('­Hola desde PaaS! Mi primera aplicaci¢n');});app.listen(port, () => {  console.log(`Servidor ejecut ndose en puerto ${port}`);});
+const express = require('express');const app = express();const port = process.env.PORT || 3000;app.get('/', (req, res) => {  res.send('­Hola desde Heroku! Mi primera aplicaci¢n PaaS');});app.listen(port, () => {  console.log(`Servidor ejecutandose en puerto ${port}`);});
